2464614e7bdc3318ffc49c38f94fa4bfee12b1ff
aimluck-ozaki <aimluck.ozaki@gmail.com>
2013-04-08 18:52:48 +0900

    ダッシュボードの一覧表示数を5件に変更する

--- src/main/scala/com/aimluck/controller/check/JsonController.scala
+++ src/main/scala/com/aimluck/controller/check/JsonController.scala
@@ -1,7 +1,6 @@
 package com.aimluck.controller.check;
 
 import com.aimluck.service.CheckService
-
 import com.aimluck.service.UserDataService
 import dispatch.json.JsValue
 import java.util.Date
@@ -12,41 +11,31 @@ import org.dotme.liquidtpl.controller.AbstractJsonDataController
 import sjson.json.JsonSerialization
 import sjson.json.JsonSerialization._
 import scala.collection.JavaConversions._
+import com.aimluck.model.Check
+import com.aimluck.controller.AbstractJsonController
 
-class JsonController extends AbstractJsonDataController {
+class JsonController extends AbstractJsonController {
   Logger.getLogger(classOf[JsonController].getName)
-
+  
   override def getList: JsValue = {
     import com.aimluck.service.CheckService.CheckListProtocol._
     val startDate: Date = new Date
-    UserDataService.fetchOne(this.sessionScope("userId")) match {
-      case Some(userData) => {
-        this.param("pageType") match{
-          case "page" => {
-        	  this.param("limit") match{
-        	  	case "10" => JsonSerialization.tojson(CheckService.fetchPageWithLimit(Some(userData), Integer.valueOf(this.param("limit"))).sortWith { (x, y) =>
-          			x.getUpdatedAt.compareTo(y.getUpdatedAt) > 0})
-        	  	case null => JsonSerialization.tojson(CheckService.fetchPageAll(Some(userData)).sortWith { (x, y) =>
-          			x.getUpdatedAt.compareTo(y.getUpdatedAt) > 0})
-        	  }
-          }
-          case "login" => {
-               this.param("limit") match{
-        	  	case "10" => JsonSerialization.tojson(CheckService.fetchLoginWithLimit(Some(userData), Integer.valueOf(this.param("limit"))).sortWith { (x, y) =>
-          			x.getUpdatedAt.compareTo(y.getUpdatedAt) > 0})
-        	  	case null => JsonSerialization.tojson(CheckService.fetchLoginAll(Some(userData)).sortWith { (x, y) =>
-          			x.getUpdatedAt.compareTo(y.getUpdatedAt) > 0})
-        	  }
-          }
-          case null =>
-          	addError(Constants.KEY_GLOBAL_ERROR, LanguageUtil.get("error.sessionError"))
-          null
-        }
-      }
-      case None =>
+    val sort = (x: Check, y: Check) =>
+      x.getUpdatedAt.compareTo(y.getUpdatedAt) > 0
+
+    JsonSerialization.tojson((for {
+      userData <- UserDataService.fetchOne(this.sessionScope("userId"))
+      pageType <- Option(this.param("pageType"))
+    } yield (userData, pageType)) match {
+      case Some((userData, "page")) =>
+        CheckService.fetchPage(Some(userData), this.param("limit")).sortWith(sort)
+      case Some((userData, "login")) => 
+        CheckService.fetchLogin(Some(userData), this.param("limit")).sortWith(sort)
+      case _ => {
         addError(Constants.KEY_GLOBAL_ERROR, LanguageUtil.get("error.sessionError"))
-        null
-    }
+        Nil
+      }
+    })
   }
 
   override def getDetail(id: String): JsValue = {
