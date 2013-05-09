50e208a7284dc416e9c03097d9067db9612befba
ippei takahashi <aimluck.ippei.takahashi@gmail.com>
2013-05-09 10:44:32 +0900

    モジュール化

--- src/main/java/com/aimluck/service/CheckDomainService.java
+++ src/main/java/com/aimluck/service/CheckDomainService.java
@@ -18,49 +18,17 @@ public class CheckDomainService {
 
   // jpのみ、whoisを取得。ただし有効期限情報はなし。
   public static String checkJp(String url) {
-
-    try {
-      InetAddress host;
-      host = InetAddress.getByName("whois.jprs.jp");
-
-      int port = 43;
-
-      Socket sock = new Socket(host, port);
-
-      BufferedReader in = new BufferedReader(new InputStreamReader(
-          sock.getInputStream(), "ISO2022JP"));
-
-      OutputStreamWriter osw = new OutputStreamWriter(sock.getOutputStream(),
-          "ISO2022JP");
-      osw.write(url + CL);
-      osw.flush();
-
-      StringBuffer sbBuf = new StringBuffer();
-      String strMes = null;
-      while ((strMes = in.readLine()) != null) {
-        sbBuf.append(strMes);
-        sbBuf.append(CL);
-      }
-      in.close();
-      sock.close();
-
-      return sbBuf == null ? "" : sbBuf.toString();
-    } catch (UnknownHostException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-      return "";
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-      return "";
-    }
+    return check("whois.jprs.jp", url);
   }
 
   public static String checkCom(String url) {
+    return check("whois.internic.net", "domain " + url);
+  }
 
+  public static String check(String hostName, String param) {
     try {
       InetAddress host;
-      host = InetAddress.getByName("whois.internic.net");
+      host = InetAddress.getByName(hostName);
 
       int port = 43;
 
@@ -71,7 +39,7 @@ public class CheckDomainService {
 
       OutputStreamWriter osw = new OutputStreamWriter(sock.getOutputStream(),
           "ISO2022JP");
-      osw.write("domain " + url + CL);
+      osw.write(param + CL);
       osw.flush();
 
       StringBuffer sbBuf = new StringBuffer();
