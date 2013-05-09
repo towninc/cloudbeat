609b341369a9259bd83b444d5d631f739a6d161d
ippei takahashi <aimluck.ippei.takahashi@gmail.com>
2013-05-09 10:40:54 +0900

    comドメインからもWhois情報を取得できるようにした

--- src/main/java/com/aimluck/service/CheckDomainService.java
+++ src/main/java/com/aimluck/service/CheckDomainService.java
@@ -14,41 +14,85 @@ import java.io.*;
 
 public class CheckDomainService {
 
-//jpのみ、whoisを取得。ただし有効期限情報はなし。
-  public static void checkJp(String url) {
+  private static String CL = System.getProperty("line.separator");
 
-	  try {
-	  InetAddress host;
-		host = InetAddress.getByName("whois.jprs.jp");
+  // jpのみ、whoisを取得。ただし有効期限情報はなし。
+  public static String checkJp(String url) {
+
+    try {
+      InetAddress host;
+      host = InetAddress.getByName("whois.jprs.jp");
 
       int port = 43;
 
       Socket sock = new Socket(host, port);
 
-      BufferedReader in = 
-          new BufferedReader(new InputStreamReader(sock.getInputStream(), "ISO2022JP"));
+      BufferedReader in = new BufferedReader(new InputStreamReader(
+          sock.getInputStream(), "ISO2022JP"));
 
-      OutputStreamWriter osw = 
-          new OutputStreamWriter(sock.getOutputStream(), "ISO2022JP");
-      osw.write(url + "\r\n");
+      OutputStreamWriter osw = new OutputStreamWriter(sock.getOutputStream(),
+          "ISO2022JP");
+      osw.write(url + CL);
       osw.flush();
 
       StringBuffer sbBuf = new StringBuffer();
       String strMes = null;
       while ((strMes = in.readLine()) != null) {
-          sbBuf.append(strMes);
-          sbBuf.append("\r\n");
+        sbBuf.append(strMes);
+        sbBuf.append(CL);
       }
       in.close();
       sock.close();
 
-	} catch (UnknownHostException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
+      return sbBuf == null ? "" : sbBuf.toString();
+    } catch (UnknownHostException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+      return "";
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+      return "";
+    }
+  }
+
+  public static String checkCom(String url) {
+
+    try {
+      InetAddress host;
+      host = InetAddress.getByName("whois.internic.net");
+
+      int port = 43;
+
+      Socket sock = new Socket(host, port);
+
+      BufferedReader in = new BufferedReader(new InputStreamReader(
+          sock.getInputStream(), "ISO2022JP"));
+
+      OutputStreamWriter osw = new OutputStreamWriter(sock.getOutputStream(),
+          "ISO2022JP");
+      osw.write("domain " + url + CL);
+      osw.flush();
+
+      StringBuffer sbBuf = new StringBuffer();
+      String strMes = null;
+      while ((strMes = in.readLine()) != null) {
+        sbBuf.append(strMes);
+        sbBuf.append(CL);
+      }
+      in.close();
+      sock.close();
+
+      return sbBuf == null ? "" : sbBuf.toString();
+    } catch (UnknownHostException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+      return "";
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+      return "";
+    }
   }
 
 }
