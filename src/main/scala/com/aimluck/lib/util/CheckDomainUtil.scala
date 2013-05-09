2a4f3d863658db7a8aa665b0546e9f64592c8055
ippei takahashi <aimluck.ippei.takahashi@gmail.com>
2013-05-09 13:33:20 +0900

    ドメインから有効期限をDate型で抜き取れるようにした

---
+++ src/main/scala/com/aimluck/lib/util/CheckDomainUtil.scala
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.aimluck.lib.util
+
+import java.util.regex.Pattern
+import java.text.SimpleDateFormat
+import java.util.Date
+import com.aimluck.service.CheckDomainService
+
+object CheckDomainUtil {
+  val JP_EXP_REG = """\[有効期限\]\s+(\d+/\d+/\d+)""".r
+  val COJP_EXP_REG = """\[状態\]\s+[\w\-\s]+?\((\d+/\d+/\d+)[,[\w\.]+]?\)""".r
+  val COM_EXP_REG = """Expiration\sDate:\s(\d+)\-([a-z]+)\-(\d+)""".r
+
+  val DATE_FORMAT = new SimpleDateFormat("yyyy/MM/dd")
+
+  val MONTH_MAP = Map("jan" -> 1, "feb" -> 2, "mar" -> 3, "apr" -> 4, "may" -> 5, "jun" -> 6, "jul" -> 7, "aug" -> 8, "sep" -> 9, "oct" -> 10, "nov" -> 11, "dec" -> 12)
+
+  def parse(whois: String) = try {
+    val parseSub = (x: Option[Date], y: String) =>
+      if (x == None) y.trim match {
+        case JP_EXP_REG(str) =>
+          Some(DATE_FORMAT.parse(str))
+        case COJP_EXP_REG(str) =>
+          Some(DATE_FORMAT.parse(str))
+        case COM_EXP_REG(str1, str2, str3) =>
+          Some(DATE_FORMAT.parse(("%s/%d/%s").format(str3, MONTH_MAP.get(str2).get, str1)))
+        case _ => None
+      }
+      else x
+    whois.split("""[\r\n|\r|\n]""").foldLeft(None: Option[Date])(parseSub)
+  } catch {
+    case _ => None
+  }
+
+  def check(url: String) =
+    if (url.endsWith("com"))
+      parse(CheckDomainService.checkCom(url))
+    else if (url.endsWith("jp"))
+      parse(CheckDomainService.checkJp(url))
+    else
+      None
+
+}
