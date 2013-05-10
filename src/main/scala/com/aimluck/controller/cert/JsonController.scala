47a343bfa48f808486f706130dcf1a6ede671793
HorinoYouhei <aimluck.horino@gmail.com>
2013-05-10 18:46:36 +0900

    SSL監視の上位10件をサマリーのログの上に表示する refs #28033

--- src/main/scala/com/aimluck/controller/cert/JsonController.scala
+++ src/main/scala/com/aimluck/controller/cert/JsonController.scala
@@ -6,24 +6,27 @@ import scala.collection.JavaConversions._
 import org.dotme.liquidtpl.controller.AbstractJsonDataController
 import org.dotme.liquidtpl.Constants
 import org.dotme.liquidtpl.LanguageUtil
-import com.aimluck.model.Check
 import com.aimluck.service.UserDataService
 import dispatch.json.JsValue
 import sjson.json.JsonSerialization._
 import sjson.json.JsonSerialization
 import com.aimluck.lib.util.BaseUtil
 import com.aimluck.service.CertCheckService
+import com.aimluck.model.CertCheck
 
 class JsonController extends AbstractJsonDataController with BaseUtil {
   Logger.getLogger(classOf[JsonController].getName)
 
   override def getList: JsValue = {
      import com.aimluck.service.CertCheckService.CertCheckListProtocol._
+     val startDate: Date = new Date
+     val sort = (x: CertCheck, y: CertCheck) => x.getUpdatedAt.compareTo(y.getUpdatedAt) > 0
+      
     JsonSerialization.tojson(UserDataService.fetchOne(this.sessionScope("userId")) match {
-      case Some(userData) => CertCheckService.fetchAll(Some(userData))
+      case Some(userData) => CertCheckService.fetchList(Some(userData), this.param("limit")).sortWith(sort)
       case None => Nil
     })
-
+    
   }
 
   override def getDetail(id: String): JsValue = {
